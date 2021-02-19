88729f69e8e5b4b49ce0957f877b76bc00666425
Adam Crandall <adamcrandall91@gmail.com>
2021-02-19 15:25:01 -0500

    created User Model and user api routes for CRUD

---
+++ controllers/user-controller.js
@@ -0,0 +1,63 @@
+const { User } = require('../models');
+
+const userController = {
+    // get all users
+    getAllUsers (req, res) {
+        User.find({})
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+        });
+    },
+
+    // get one user by id
+    getUserById ({ params }, res) {
+        User.findOne({ _id: params.id})
+        .then(dbUserData => {
+            if(!dbUserData) {
+                res.status(404).json({ message: 'No user found with this id 🙃'});
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+        });
+    },
+
+    // create a User
+    createUser ({ body }, res) {
+        User.create(body)
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => res.status(400).json(err));
+    },
+
+    // update a User by id
+    updateUser ({ params, body }, res) {
+        User.findOneAndUpdate({ _id: params.id }, body, { new: true })
+        .then(dbUserData => {
+            if(!dbUserData) {
+                res.status(404).json({ message: 'No user found with this id 🙃'})
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => res.status(400).json(err));
+    },
+
+    // Delete a user by id
+    deleteUser ({ params }, res) {
+        User.findOneAndDelete({ _id: params.id })
+        .then(dbUserData => {
+            if (!dbUserData) {
+                res.status(404).json({ message: 'No user found with this id 🙃'});
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => res.status(400).json(err));
+    }
+};
+
+module.exports = userController;
---
+++ models/User.js
@@ -0,0 +1,39 @@
+const { Schema, model } = require('mongoose');
+
+const UserSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: [/.+@.+\..+/]
+    },
+    thoughts: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Thought'
+        }
+    ],
+    friends: []
+},
+{
+    toJSON: {
+        virtuals: true,
+        getters: true
+    },
+    id: false
+}
+);
+
+UserSchema.virtual('friendCount').get(function() {
+    return this.friends.reduce((total, friend) => total + friend.length + 1, 0);
+});
+
+const User = model('User', UserSchema);
+
+module.exports = User;
